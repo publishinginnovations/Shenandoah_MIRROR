dd02ea4f020b0abaacea1b6c0b9b85350ae0b6d6
Your Name <you@example.com>
2019-04-30 13:48:11 -0400

    ALL_TEST

---
+++ shenandoahliterary.org/622/wp-content/themes/twentyten_issue_4/js/accordion3c21.js
@@ -0,0 +1,21 @@
+jQuery(function() {
+		jQuery( "#accordion2" ).accordion({
+			collapsible: true,
+            autoHeight: false,
+            navigation: true,
+            active: 0,
+            header: "h3"
+            });
+
+	});	
+	
+jQuery(function() {
+		jQuery( "#accordion" ).accordion({
+			collapsible: true,
+            autoHeight: false,
+            navigation: true,
+            active: false,
+            header: "h3"
+            });
+
+	});	
